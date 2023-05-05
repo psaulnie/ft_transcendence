c1fe1c79535893c9d5769d1810ae044c2de4f2fd
pierrot <saulnier254@gmail.com>
2023-05-05 10:35:57 +0200

    now react is in typescript (need to retest the api calls)

--- data/frontend/src/App.tsx
+++ data/frontend/src/App.tsx
@@ -1,30 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [users, setUsers] = useState([])
-
-  const fetchUserData = () => {
-    fetch("http://localhost:5000/users")
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setUsers(data)
-      })
-  }
-
-  useEffect(() => {
-    fetchUserData()
-  }, [])
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>{users[0]}</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
