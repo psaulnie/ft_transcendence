244a3614f9e7940739027a5163a8ce35a9ec4485
pierrot <saulnier254@gmail.com>
2023-05-19 17:36:50 +0200

    join &  left room

--- data/frontend/src/components/Chat/Messages.tsx
+++ data/frontend/src/components/Chat/Messages.tsx
@@ -8,8 +8,31 @@ export default function Messages({ messages }: arg) {
 	return (
 		<div className='messages'>
 		{
-		messages.map((message, index) =>
-			<p key={ index }>{ message }</p>
+		messages.map((message, index) => {
+			const arr = message.split(' ');
+
+			if (arr[1] == "JOIN")
+			{
+				return(
+					<div className='userJoined'> {/*the three paragraphs must be on the same line*/}
+						<p>=&gt; </p>
+						<p>{arr[0]}</p>
+						<p> joined the room</p>
+					</div>
+				);
+			}
+			else if (arr[1] == "LEFT")
+			{
+				return(
+					<div className='userLeft'> {/*the three paragraphs must be on the same line*/}
+						<p>&#60;= </p>
+						<p>{arr[0]}</p>
+						<p> left the room</p>
+					</div>
+				);
+			}
+			return (<p key={ index }>{ message }</p>);
+		}
 		)
 		}
 		</div>
