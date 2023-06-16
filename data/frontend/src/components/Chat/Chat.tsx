2dcc79a1bbe985a3f4ad11d953a7d845916e2b2c
pierrot <saulnier254@gmail.com>
2023-06-16 10:46:14 +0200

    frontend fix

--- data/frontend/src/components/Chat/Chat.tsx
+++ data/frontend/src/components/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { chatSocket } from '../../chatSocket';
 import { manageRoomsTypes } from './args.types';
