e53d652baf28c13873d2a14d54cd2975cce6d9c1
Delphine <dbouron@student.42lyon.fr>
2023-06-27 11:36:26 +0200

    Add 2 buttons to test auth in frontend

--- data/backend/src/auth/auth.module.ts
+++ data/backend/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '180s' },
     }),
     PassportModule,
   ],
--- data/frontend/src/App.tsx
+++ data/frontend/src/App.tsx
@@ -7,36 +7,82 @@ import Chat from './components/Chat/Chat';
 
 function App() {
 	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 	const [showChat, setShowChat] = useState(false);
+	const [apiToken, setApiToken] = useState('');
 
-	function onChange(e: React.FormEvent<HTMLInputElement>)
-	{
+	const signInRequestOptions = {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ username: username, password: password })
+	};
+
+	const profileRequestOptions = {
+		headers: { 'Authorization': `Bearer ${apiToken}` }
+	};
+
+	//Get Method
+	const apiGet = () => {
+		fetch("http://localhost:5000/auth/login", signInRequestOptions)
+			.then((response) => response.json())
+			.then((json) => {
+				console.log(json);
+				setApiToken(json.access_token);
+			});
+	};
+
+	const apiGetProfile = () => {
+		console.log(profileRequestOptions)
+		fetch("http://localhost:5000/profile", profileRequestOptions)
+			.then((response) => response.json())
+			.then((json) => {
+				console.log(json);
+			});
+	};
+
+	function onUsernameChange(e: React.FormEvent<HTMLInputElement>) {
 		e.preventDefault();
 		setUsername(e.currentTarget.value);
 	}
 
-	function onSubmit(e: SyntheticEvent)
-	{
+	function onPasswordChange(e: React.FormEvent<HTMLInputElement>) {
+		e.preventDefault();
+		setPassword(e.currentTarget.value);
+	}
+
+	function onSubmit(e: SyntheticEvent) {
 		e.preventDefault();
 		if (username != '')
 			setShowChat(true);
 	}
 
+	function signIn(e: SyntheticEvent) {
+		e.preventDefault();
+		apiGet();
+	}
+
+	function logIn(e: SyntheticEvent) {
+		e.preventDefault();
+		apiGetProfile();
+	}
+
 	return (
-	<div className="App"> {/* TODO rename */}
-		<Navigation />
-		<div className='main'>
-			<form onSubmit={onSubmit}>
-				<p>Username:</p>
-				<input value={username} onChange={ onChange } />
-				<button>Submit</button>
-			</form>
-		{
-			showChat ? (<Chat username={username} />) : ''
-		}
-		{/* TODO add Game component*/}
+		<div className="App"> {/* TODO rename */}
+			<Navigation />
+			<div className='main'>
+				<form>
+					<input value={username} onChange={onUsernameChange} />
+					<input value={password} onChange={onPasswordChange} />
+					<button onClick={signIn}>SignIn</button>
+					<button onClick={logIn}>LogIn</button>
+				</form>
+
+				{
+					showChat ? (<Chat username={username} />) : ''
+				}
+				{/* TODO add Game component*/}
+			</div>
 		</div>
-	</div>
 	);
 }
 
