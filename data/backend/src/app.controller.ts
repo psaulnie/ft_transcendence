d952c8235ab47892326be4e9c14ee8643c91c8eb
psaulnie <saulnier254@gmail.com>
2023-08-01 16:24:34 +0200

    check file type

--- data/backend/src/app.controller.ts
+++ data/backend/src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Body, Post, Query, UploadedFile } from '@nestjs/common';
 import { UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { HttpException } from '@nestjs/common'; 
+import { ParseFilePipe, FileTypeValidator } from '@nestjs/common';
 
 import { diskStorage } from 'multer';
 import Path = require('path');
@@ -10,7 +11,18 @@ import { randomUUID } from 'crypto';
 import { UserService } from './services/user.service';
 import { AppService } from './app.service'; 
 
-const storage = {
+
+const fileInterceptorOptions = {
+	fileFilter: (req, file, cb) => {
+		const validator = new FileTypeValidator({fileType: /\.(jpg|jpeg|png|gif)$/});
+
+		console.log(validator.isValid(file));
+		if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png' || file.mimetype === 'image/gif')
+			cb(null, true);
+		else {
+			cb(null, false);
+		}
+	},
 	storage: diskStorage({
 		destination: '/avatars',
 		filename: (req, file, cb) => {
@@ -26,17 +38,19 @@ export class AppController {
 	constructor(private readonly appService: AppService, private readonly userService: UserService) {}
 
 	@Post('/avatar')
-    @UseInterceptors(FileInterceptor('file', storage))
+    @UseInterceptors(
+		FileInterceptor('file', fileInterceptorOptions))
 	async uploadAvatar(@Body() body: any, @UploadedFile() file: Express.Multer.File) { // TODO add access token
 		if (body && file && body.username) {
 			const user = await this.userService.findOne(body.username);
 			if (user) {
+				console.log("upload");
 				await this.userService.updateAvatar(user, file.path);
 			}
 			else
 				throw new HttpException('Unprocessable Entity', 422);
 		}
-		console.log(body.username)
-		console.log(file);
+		else
+			throw new HttpException('Unprocessable Entity', 422);
 	}
 }
